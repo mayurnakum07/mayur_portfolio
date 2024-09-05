921affa62d56dc962a0ed45bd7bdb3bef3e2423f
freelancework00700 <freelancework00700@gmail.com>
2024-09-05 17:15:47 +0530

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <main className="min-h-screen flex flex-col p-20">
         <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-        <p>Hey, I'm Mayur 👋🏻</p>
+        <p>Hey, {`I'm`} Mayur 👋🏻</p>
       </main>
     </div>
   );
