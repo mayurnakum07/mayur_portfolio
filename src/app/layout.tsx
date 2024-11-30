45f0b43a24edf899c0a13d434e3d28a09114f3a4
mayurnakum07 <mayurnakum0000@gmail.com>
2024-11-30 20:27:11 +0530

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import mainIMG from "../../public/assets/mainImg.png";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
