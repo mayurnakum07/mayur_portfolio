81636ea05d7811f81ed85625e7c518cdf29f25ab
Mayur <mayur.disolutions@gmail.com>
2024-12-03 10:01:47 +0530

    implemented analytics extention

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Mayur | Frontend Web Developer Portfolio",
@@ -47,6 +48,7 @@ export default function RootLayout({
     <>
       <html lang="en">
         <SpeedInsights />
+        <Analytics />
         <body>
           <Header />
           {children}
